0c5c7e030d6139dd827ab6364ecd0ae133f5e790
fera zodd <tom@bkfam.nl>
2026-03-06 17:10:50 +0100

    read in text file n print

--- file2hex/hex.c
+++ file2hex/hex.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 1000
 
 int main() {
 
+    FILE* fileIn;
+    char str[MAX];
     char filePath[] = "";
     
     printf("enter file path: ");
     scanf("%s", filePath);
 
-    printf("file path is: %s\n", filePath);
+    fileIn = fopen(filePath, "r");
+
+    printf("file content is: \n");
+
+    while(fgets(str, 100, fileIn)){
+        printf("%s", str);
+    }    
+    
+    fclose(fileIn);
 
     return 0;
 }
